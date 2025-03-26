bab01584149d32a37eaa00b2d67583c17c834d33
Robert Thornberry <107797295+Rob-Notes@users.noreply.github.com>
2025-03-26 15:45:09 +0000

    added speech to text file
    should work with pc with mic

---
+++ SpeechToText.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+import pyttsx3
+
+
+r = sr.Recognizer()
+
+def record_text():
+    while True:
+        try:
+            with sr.Microphone() as source2:
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+
+                audio2 = r.listen(source2)
+
+                MyText = r.recognize_google(audio2)
+
+                return MyText
+
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+
+        except sr.UnknownValueError:
+            print("Unknown error occurred")
+
+while True:
+    text = record_text()
+    print("Recognized Text:", text)
